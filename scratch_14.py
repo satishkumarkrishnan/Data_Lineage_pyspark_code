4ffabd2766b5f07288fdd7231c4d1c6af6a7d381
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-26 12:43:26 +0530

    updated pyspark code

--- scratch_14.py
+++ scratch_14.py
@@ -1,24 +1,50 @@
+import json
+import pandas
+from pyspark.shell import sc
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+import boto3
 
-if __name__ == "__main__":
-    spark = SparkSession.builder \
-        .appName("PySpark OpenLineage Example") \
-        .config("openlineage.url", "http://localhost:5000")  \
-        .config("openlineage.backend", "io.openlineage.spark.agent.backends.ConsoleLoggingBackend") \
-        .config("openlineage.namespace", "test") \
-        .getOrCreate()
+# from boto3_fixtures.contrib import boto3
+jar_path = "openlineage-spark_2.12-1.13.1.jar"
+spark = SparkSession.builder.getOrCreate()
+sc.setLogLevel("DEBUG")
+spark = (SparkSession.builder.master('local').appName('Python Spark SQL basic example')
+         .config('spark.jars', jar_path)
+         .config('spark.some.config.option', 'some-value')
+         .config('spark.openlineage.transport.type', 'http')
+         .config('spark.extraListeners', 'io.openlineage.spark.agent.OpenLineageSparkListener')
+         .config('spark.openlineage.transport.type', 'http')
+         .config('spark.openlineage.transport.url', 'http://localhost:8080')
+         .config('spark.openlineage.namespace', 'spark_namespace')
+         .config('spark.openlineage.parentJobNamespace', 'airflow_namespace')
+         .config('spark.openlineage.parentJobName', 'airflow_dag.airflow_task')
+         .config('spark.openlineage.parentRunId', 'xxxx-xxxx-xxxx-xxxx')
+         .getOrCreate())
 
-    # Example PySpark job
-    df = spark.read.text("test.csv")
-    df.show()
+# Your Spark job code
+input_path = "test.csv"
+output_path = "output.csv"
+df = spark.read.csv(input_path, header=True)
+third_column = df.columns[2]
+age = 40
+filtered_df = df.filter(df[third_column] >= age)
+filtered_df.show()
+filtered_df.write.csv("filtered.csv", header=True)
+lineage_data = {
+    "job_name": "YourSparkJob",
+    "inputs": [input_path],
+    "outputs": [output_path],
+}
+lineage_json = json.dumps(lineage_data)
+# Initialize Glue client
+#glue = boto3.client('glue')
 
-    # Perform some transformation
-    third_column = df.columns[2]
-    age = 40
-    filtered_df = df.filter(df["age"] >= 40)
-    filtered_df.show()
+# Example S3 location to store lineage information
+# s3_bucket = 's3://ddsl-extension-bucket/'
+# s3_key = 'lineage.json'
 
-    # Write data
-    filtered_df.write.mode('overwrite').csv("output.csv")
+# Upload lineage JSON to S3
+# glue.put_data_catalog_lineage_settings(CatalogId='your-catalog-id', LineageSettings={"LineageData": lineage_json})
 
-    spark.stop()
+spark.stop()
